38ad6ec8f9488604fbf10d5eeedd6a2dbb4e346d
fg2310 <62403481+fg2310@users.noreply.github.com>
2025-07-27 04:57:23 +0200

    Delete orcid-api-common directory

--- orcid-api-common/src/main/java/org/orcid/api/common/analytics/client/AnalyticsClient.java
+++
@@ -1,8 +0,0 @@
-package org.orcid.api.common.analytics.client;
-
-import org.orcid.api.common.analytics.AnalyticsData;
-
-public interface AnalyticsClient {
-
-    void sendAnalyticsData(AnalyticsData data);
-}
